db6779f5a991aad19105b0d4d00574bf794782c2
Luiz Felipe <lffm1994@gmail.com>
2022-11-27 22:33:23 -0300

    Criacao do projeto.

---
+++ Webflux/src/test/java/com/schoolofnet/Webflux/WebfluxApplicationTests.java
@@ -0,0 +1,13 @@
+package com.schoolofnet.Webflux;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebfluxApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
