fb7cdb40c5c3a5f78695073b816178f647fcdffb
Luiz Felipe <lffm1994@gmail.com>
2022-11-29 01:28:47 -0300

    Configurando reactive com jpa

---
+++ Webflux/src/main/java/com/schoolofnet/webflux/controller/TodoController.java
@@ -0,0 +1,33 @@
+package com.schoolofnet.webflux.controller;
+
+import com.schoolofnet.webflux.model.Todo;
+import com.schoolofnet.webflux.repository.TodoRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.transaction.support.TransactionTemplate;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import reactor.core.publisher.Mono;
+import reactor.core.scheduler.Scheduler;
+
+@RestController
+@RequestMapping("/todos")
+public class TodoController {
+
+    @Autowired
+    private TodoRepository todoRepository;
+
+    @Autowired
+    private TransactionTemplate transactionTemplate;
+
+    @Autowired
+    @Qualifier("jdbcSchedule")
+    private Scheduler jdbcScheduler;
+
+    @PostMapping
+    public Mono<Todo> save(@RequestBody Todo todo) {
+      //  return this.todoRepository.save(todo);
+    }
+}
--- Webflux/src/main/java/com/schoolofnet/webflux/repository/TodoRepository.java
+++ Webflux/src/main/java/com/schoolofnet/webflux/repository/TodoRepository.java
@@ -1,9 +1,9 @@
 package com.schoolofnet.webflux.repository;
 
 import com.schoolofnet.webflux.model.Todo;
-import org.springframework.data.repository.reactive.ReactiveCrudRepository;
+import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface TodoRepository extends ReactiveCrudRepository<Todo, Long> {
+public interface TodoRepository extends CrudRepository<Todo, Long> {
 }
