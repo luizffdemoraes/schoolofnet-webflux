605176b574cdfe7528ba19fa0ebd265706d92869
Luiz Felipe <lffm1994@gmail.com>
2022-11-29 23:26:15 -0300

    Trabalhando com listagem unica

--- Webflux/src/main/java/com/schoolofnet/webflux/controller/TodoController.java
+++ Webflux/src/main/java/com/schoolofnet/webflux/controller/TodoController.java
@@ -5,12 +5,11 @@ import com.schoolofnet.webflux.repository.TodoRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.transaction.support.TransactionTemplate;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import reactor.core.publisher.Mono;
 import reactor.core.scheduler.Scheduler;
+
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/todos")
@@ -25,6 +24,12 @@ public class TodoController {
     @Autowired
     @Qualifier("jdbcSchedule")
     private Scheduler jdbcScheduler;
+
+    @GetMapping("/{id}")
+    @ResponseBody
+    public Mono<Optional<Todo>> findById(@PathVariable("id") Long id){
+        return Mono.just(this.todoRepository.findById(id));
+    }
 
     @PostMapping
     public Mono<Todo> save(@RequestBody Todo todo) {
